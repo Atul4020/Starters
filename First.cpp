170a49848559e70893c9c85a084ba58468f24e03
Niteeshkumar07 <niteeshkumar4020@gmail.com>
2025-07-31 15:38:55 +0530

    what is happening

--- First.cpp
+++ First.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
     cout<<"hello you are first mate";
-    cout<<"now new branch come";
+    cout<<"deleted firts branch data now we change it more";
+    cout<<"we do some stuff here";
     return 0;
 }
