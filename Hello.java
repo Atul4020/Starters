0c45d8efeb0e50c1de28227687c973db8e35dcb6
Niteeshkumar07 <niteeshkumar4020@gmail.com>
2025-08-03 00:22:04 +0530

    yup this comes interseting

--- Hello.java
+++ Hello.java
@@ -2,4 +2,4 @@ public class Hello {
     public static void main(StringDemo[] args) {
         System.out.println("hello world");
     }
-}  //hello i made the chnages can you want to make permanent or nto
+}  //hello i made the chnages can you want to make permanent or not hahaa what are you doing mate~ 
